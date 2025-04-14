02fc21c0ec7b34740757c591be55e094f16b8614
pinpox <github@pablo.tools>
2025-04-14 11:09:40 +0200

    Update example

--- cmd/age-plugin-bitwarden/main.go
+++ cmd/age-plugin-bitwarden/main.go
@@ -10,14 +10,14 @@ func main() {
 
 	var example = `
   $ age-plugin-bitwarden --print-recipients
-  op://Personal/SSH key/public key: ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINKZfejb9htpSB5K9p0RuEowErkba2BMKaze93ZVkQIE
+  ce3e36d1-271c-4019-9679-3244c41797e7 (person1@bitwarden): ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINKZfejb9htpSB5K9p0RuEowErkba2BMKaze93ZVkQIE
+
 
   $ echo "Hello World" | age -r "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINKZfejb9htpSB5K9p0RuEowErkba2BMKaze93ZVkQIE" > secret.age
 
   $ age --decrypt -j bitwarden -o - secret.age
   Hello World`
 
-	log.Println("Using Bitwarden as backend")
 	backend := Bitwarden{}
 	rootCmd := pwmanager.RootCmd(backend, example)
 
